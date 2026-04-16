7de623fcb1946e2de2873a7f596e35920a975936
fun02 <emailkamu@gmail.com>
2026-04-16 19:18:48 +0700

    Tes jalur anti-gelap

--- routes/web.php
+++ routes/web.php
@@ -12,4 +12,7 @@ Route::get('/', function () {
 Route::post('/ai/summarize', [AiController::class, 'summarize']);
 Route::post('/ai/ask', [AiController::class, 'ask']);
 Route::post('/auth/send-otp', [AiController::class, 'sendOtpEmail']);
+Route::get('/tes-server', function () {
+    return '<h1 style="color: black; background: white; padding: 50px;">SERVER LARAVEL 100% BERHASIL DAN SEHAT!</h1>';
+});
 
